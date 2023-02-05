4a1e8f63df6debd81fdb02256479857f7827ffab
Marcelo Scofano Diniz <marceloscofano@integraremed.com>
2023-02-05 02:11:01 -0300

    trying to implement improvements

--- src/App.js
+++ src/App.js
@@ -1,103 +1,11 @@
+// https://www.geeksforgeeks.org/how-to-build-a-tic-tac-toe-game-using-react-hooks/
 // https://beta.reactjs.org/learn/tutorial-tic-tac-toe
+// https://github.com/thiskevinwang/react-tictactoe-extras
+// https://codepen.io/msdiniz-the-sasster/pen/LYBaGRW?editors=0110
+// import { useReducer } from "react";
 import { useState } from "react";
 import React from "react";
-// function Square() {
-// const [value, setValue] = useState(null);
-// Commented because I Lifted the state to Board component
-// function handleClick() {
-//   setValue('X');
-// }
-
-// function handleClick() {
-//   console.log(`${value} clicked!`);
-// }
-
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
-// Array(9).fill(null) creates an array with nine elements and
-// sets each of them to null.
-// The useState() call around it declares a squares state variable that’s initially set to that array. Each entry in the array corresponds to the value of a square. When you fill the board in later, the squares array will look something like this:
-// ['O', null, 'X', 'X', 'X', 'O', 'O', null, null]
-// function Board() {
-//   const [xIsNext, setXIsNext] = useState(true);
-//   const [squares, setSquares] = useState(Array(9).fill(null));
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-    // setSquares(nextSquares);
-    // setXIsNext(!xIsNext);
-    onPlay(nextSquares);
-    console.log(nextSquares);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-  return (
-    // <>
-    // ^^^^ not working because Babel version:
-    // https://stackoverflow.com/questions/48316365/react-fragment-shorthand-failing-to-compile
-    <React.Fragment>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-      {/* {status.match("Winner") && (
-        <button onClick={handleNewGame}>New Game ?</button>
-      )} */}
-    </React.Fragment>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+import Board from "./Board";
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
@@ -107,9 +15,20 @@ export default function Game() {
   // const currentSquares = history[history.length - 1];
   // Always the last move ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   const currentSquares = history[currentMove];
-  
+  const [winnerCoordinates, setWinnerCoordinates] = useState(null);
+
+  // https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
+  // const [ignored, forceUpdate] = useReducer(x => x + 1, 0);    
+  // const [seed, setSeed] = useState(1);
+  // const reset = () => {
+  //   setSeed(Math.random());
+  // }
   function handleNewGame() {
-    setHistory(Array(9).fill(null));
+    // reset();
+    window.location.reload();
+    // forceUpdate();
+    // setHistory(Array(9).fill(null));
+    // setCurrentMove(0);
     // setXIsNext(true);
   }
 
@@ -125,6 +44,38 @@ export default function Game() {
     // setXIsNext(nextMove % 2 === 0);
   }
 
+  function calculateWinner(squares) {
+    if (winnerCoordinates) return;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        setWinnerCoordinates([a, b, c]);
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(currentSquares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else if (history.length === 10) {
+    status = 'Draw! ';
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
@@ -134,19 +85,28 @@ export default function Game() {
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move !== currentMove && <button onClick={() => jumpTo(move)}>{description}</button>}
+        {move === currentMove && <b>`You are at move #{move}`</b>}        
       </li>
     );
   });
 
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <React.Fragment>
+      <div className="status">{status}</div>
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} someoneWon={winner} winnerCordinates={winnerCoordinates} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+        <div>
+          {(status.match("Winner")|| status.match("Draw")) && (
+            <button onClick={handleNewGame}>New Game ?</button>
+          )}
+        </div>
       </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
---
+++ src/Board.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+// function Square() {
+// const [value, setValue] = useState(null);
+// Commented because I Lifted the state to Board component
+// function handleClick() {
+//   setValue('X');
+// }
+// function handleClick() {
+//   console.log(`${value} clicked!`);
+// }
+function Square({ value, onSquareClick, highlight = false }) {
+  return (
+    <button className={"square" + (highlight ? " highlight" : "")} onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+// Array(9).fill(null) creates an array with nine elements and
+// sets each of them to null.
+// The useState() call around it declares a squares state variable that’s initially set to that array. Each entry in the array corresponds to the value of a square. When you fill the board in later, the squares array will look something like this:
+// ['O', null, 'X', 'X', 'X', 'O', 'O', null, null]
+// function Board() {
+//   const [xIsNext, setXIsNext] = useState(true);
+//   const [squares, setSquares] = useState(Array(9).fill(null));
+
+export default function Board({ xIsNext, squares, onPlay, someoneWon, winnerCoordinates }) {
+  function handleClick(i) {
+    if (squares[i] || someoneWon) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
+    onPlay(nextSquares);
+    console.log(nextSquares);
+  }
+  function shouldHighlight(whichSquare) {
+    if (!someoneWon) return false;
+    console.log(winnerCoordinates);
+    return winnerCoordinates.indexOf(whichSquare) !== -1;
+  }
+  
+  function renderSquare(i) {
+    return (
+      <Square
+        key={"square " + i}
+        highlight={shouldHighlight(i)}
+        value={squares[i]}
+        onSquareClick={() => handleClick(i)}
+      />
+    );
+  }
+  // highlight={shouldHighlight(0)} value={squares[0]} onSquareClick={() => handleClick(0)}
+
+  function renderSquares(n) {
+    let squares = [];
+    for (let i = n; i < n + 3; i++) {
+      squares.push(renderSquare(i));
+    }
+    return squares;
+  }
+
+  function renderRows(i) {
+    return <div className="board-row" key={"div " + i}>{renderSquares(i)}</div>;
+  }
+
+  return (
+    // <>
+    // ^^^^ not working because Babel version:
+    // https://stackoverflow.com/questions/48316365/react-fragment-shorthand-failing-to-compile
+    <React.Fragment>
+      {/* <div className="board-row">
+        <Square highlight={shouldHighlight(0)} value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square highlight={shouldHighlight(1)} value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square highlight={shouldHighlight(2)} value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square highlight={shouldHighlight(3)} value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square highlight={shouldHighlight(4)} value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square highlight={shouldHighlight(5)} value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square highlight={shouldHighlight(6)} value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square highlight={shouldHighlight(7)} value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square highlight={shouldHighlight(8)} value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div> */}
+      {renderRows(0)}
+      {renderRows(3)}
+      {renderRows(6)}
+    </React.Fragment>
+
+  );
+}
